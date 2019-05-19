6b36cc7dfb7ccf9b1bd4429bc84c0cfb0466004e
sanekua <sasha-kuzmyk@mail.ru>
2019-05-19 11:56:39 +0300

    Web form

---
+++ web/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request,'web/home.html',context)
+
+
+
+def about(request):
+    return render(request,'web/about.html',{'title':'About'})
